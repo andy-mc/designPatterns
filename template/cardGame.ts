c68ef1de37dafbe8603adf439b6d23d04df28e20
Andrés Muñoz <andres.munoz@idukay.com>
2023-11-03 00:18:03 -0500

    improve cardGame

--- template/cardGame.ts
+++ template/cardGame.ts
@@ -1,25 +1,37 @@
-class Pockemon {
-  public name: string
-  public attack: number
-  public health: number
+class Pokemon {
+  public name: string;
+  public attack: number;
+  private _health: number;
 
   constructor(name: string, attack: number, health: number) {
     this.name = name;
     this.attack = attack;
-    this.health = health;
+    this._health = health;
+  }
+
+  get health(): number {
+    return this._health;
+  }
+
+  takeDamage(damage: number): void {
+    this._health = Math.max(0, this._health - damage);
+  }
+
+  increaseHealth(health: number): void {
+    this._health = health;
   }
 }
 
 abstract class CardGame {
-  protected creatures: Pockemon[]
+  protected creatures: Pokemon[]
 
-  constructor(creatures: Pockemon[]) {
+  constructor(creatures: Pokemon[]) {
     this.creatures = creatures;
   }
 
-  abstract hit(attacker: Pockemon, defender: Pockemon): void;
-  
-  combat(creature1index: number, creature2index: number) {
+  abstract hit(attacker: Pokemon, defender: Pokemon): void;
+
+  combat(creature1index: number, creature2index: number): string {
     const first = this.creatures[creature1index];
     const second = this.creatures[creature2index];
     this.hit(first, second);
@@ -27,40 +39,34 @@ abstract class CardGame {
     const firstAlive = first.health > 0;
     const secondAlive = second.health > 0;
     if (firstAlive === secondAlive) return "tie";
-    return firstAlive ? this.creatures[creature1index].name : this.creatures[creature2index].name
+    return firstAlive ? first.name : second.name;
   }
 }
 
 class TemporaryCardDamageGame extends CardGame {
-  constructor(creatures: Pockemon[]) {
-    super(creatures);
-  }
-
-  hit(attacker: Pockemon, defender: Pockemon) {
-    if (attacker.attack >= defender.health) {
-      defender.health = 0;
+  hit(attacker: Pokemon, defender: Pokemon): void {
+    const originalHealth = defender.health;
+    defender.takeDamage(attacker.attack);
+    if (defender.health <= 0) {
+      defender.increaseHealth(originalHealth);
     }
   }
 }
 
 class PermanentCardDamageGame extends CardGame {
-  constructor(creatures: Pockemon[]) {
-    super(creatures);
-  }
-
-  hit(attacker: Pockemon, defender: Pockemon) {
-    defender.health = Math.max(0, defender.health - attacker.attack);
+  hit(attacker: Pokemon, defender: Pokemon): void {
+    defender.takeDamage(attacker.attack);
   }
 }
 
-const c1 = new Pockemon("pickashu", 1, 2);
-const c2 = new Pockemon("charmander", 1, 3);
-const gamePockemon = new TemporaryCardDamageGame([c1, c2]);
-console.log(gamePockemon.combat(0, 1));
-console.log(gamePockemon.combat(0, 1));
+const c1 = new Pokemon("pickashu", 1, 2);
+const c2 = new Pokemon("charmander", 1, 3);
+const gamePokemon = new TemporaryCardDamageGame([c1, c2]);
+console.log(gamePokemon.combat(0, 1));
+console.log(gamePokemon.combat(0, 1));
 
-const c12 = new Pockemon("pickashu", 1, 2);
-const c22 = new Pockemon("charmander", 1, 3);
-const gamePockemon2 = new PermanentCardDamageGame([c12, c22]);
-console.log(gamePockemon2.combat(0, 1));
-console.log(gamePockemon2.combat(0, 1));
+const c12 = new Pokemon("pickashu", 1, 2);
+const c22 = new Pokemon("charmander", 1, 3);
+const gamePokemon2 = new PermanentCardDamageGame([c12, c22]);
+console.log(gamePokemon2.combat(0, 1));
+console.log(gamePokemon2.combat(0, 1));
