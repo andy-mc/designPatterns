510b8059048ee34385b45a804cb3d0fa34470f88
Andrés Muñoz <andres.munoz@idukay.com>
2023-11-05 13:57:50 -0500

    define price discount probelm

---
+++ src/_discountPriceProblem.ts
@@ -0,0 +1,73 @@
+// // Problema: Calculo de descuento para diferentes productos
+
+// function calcularDescuento(precio: number, descuento: number) {
+//   return precio - precio * descuento;
+// }
+
+// const DESCUENTO = 0.1; // 10% de descuento para todos los productos
+
+// function calcularDescuentoProductoA(precio: number) {
+//   return calcularDescuento(precio, DESCUENTO);
+// }
+
+// function calcularDescuentoProductoB(precio: number) {
+//   return calcularDescuento(precio, DESCUENTO);
+// }
+
+// function calcularDescuentoProductoC(precio: number) {
+//   return calcularDescuento(precio, DESCUENTO);
+// }
+
+// console.log(calcularDescuentoProductoA(100));
+// console.log(calcularDescuentoProductoB(200));
+// console.log(calcularDescuentoProductoC(300));
+
+
+// // Solución: Patrón Strategy
+
+// interface IDiscountStrategy {
+//   applyDiscount(price: number): number;
+// }
+
+// class DiscountByPercentage implements IDiscountStrategy {
+//   constructor(private discountRate: number) {}
+
+//   applyDiscount(price: number): number {
+//     return price - (price * this.discountRate);
+//   }
+// }
+
+// class DiscountHalfPrice implements IDiscountStrategy {
+//   applyDiscount(price: number): number {
+//     return price / 2;
+//   }
+// }
+
+// class Product {
+//   constructor(
+//     private _name: string, 
+//     private _basePrice: number,
+//     private discountStrategy: IDiscountStrategy
+//   ) { }
+  
+//   get name(): string {
+//     return this._name;
+//   }
+
+//   get basePrice(): number {
+//     return this._basePrice;
+//   }
+
+//   get priceAfterDiscount(): number {
+//     return this.discountStrategy.applyDiscount(this._basePrice);
+//   }
+// }
+
+// const tenPercentDiscount = new DiscountByPercentage(0.10); // 10% descuento como 0.10
+// const halfPriceDiscount = new DiscountHalfPrice();
+
+// const product = new Product('Laptop', 1000, tenPercentDiscount);
+// console.log(`${product.name}: $${product.priceAfterDiscount}`); // Laptop: $900
+
+// const productB = new Product('Laptop', 1000, halfPriceDiscount);
+// console.log(`${productB.name}: $${productB.priceAfterDiscount}`); // Laptop: $500
